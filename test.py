256ec9dc86a32528872d2e5e67d8345d03ea9390
martnreh <martinbarrientos451@gmail.com>
2024-03-13 07:37:48 -0600

    ..

--- test.py
+++ test.py
@@ -1,42 +1,47 @@
 import os
 import pandas as pd
 import requests
+
+
 from PIL import Image
 from io import BytesIO
 
-def download_image(url, sku):
+
+#Adrian Martin Hernandez Barrientos 
+
+def descargarImagen(url, sku):
     try:
         if url:
             response = requests.get(url)
             if response.status_code == 200:
-                image = Image.open(BytesIO(response.content))
-                image = image.convert("RGB")
-                image.thumbnail((800, 800))
+                imagen = Image.open(BytesIO(response.content))
+                imagen = imagen.convert("RGB")
+                imagen.thumbnail((800, 800))
                 output = BytesIO()
-                image.save(output, format='JPEG', quality=85)
+                imagen.save(output, format='JPEG', quality=85)
                 if len(output.getvalue()) <= 100 * 1024: 
                     with open(f"imagenes/{sku}.jpg", "wb") as f:
                         f.write(output.getvalue())
-                    print(f"Imagen guardada para SKU {sku}")
+                    print(f"Imagen guardada con SKU {sku}")
                 else:
-                    print(f"La imagen para SKU {sku} excede los 100KB y no se guardará.")
+                    print(f"Imagen con SKU {sku} excede los 100KB y no se va a guardar.")
             else:
-                print(f"No se pudo descargar la imagen para SKU {sku}")
+                print(f"Error al descargar la imagen de SKU {sku}")
         else:
-            print(f"No hay URL de imagen para SKU {sku}")
+            print(f"No hay URL de imagen de SKU {sku}")
     except Exception as e:
-        print(f"Error al descargar o procesar la imagen para SKU {sku}: {e}")
+        print(f"Error al procesar la imagen para SKU {sku}: {e}")
 
 def main():
-    catalog = pd.read_excel("catalogo.xlsx")
+    catalogo = pd.read_excel("catalogo.xlsx")
 
     if not os.path.exists("imagenes"):
         os.makedirs("imagenes")
 
-    for index, row in catalog.iterrows():
+    for index, row in catalogo.iterrows():
         sku = row["SKU"]
         url = row["Url"]
-        download_image(url, sku)
+        descargarImagen(url, sku)
 
 if __name__ == "__main__":
     main()
